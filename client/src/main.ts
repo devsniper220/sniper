365d27d247ab74ae2b9ffbd7025f6676cb8cc85e
devsniper1106 <andyblake1106@gmail.com>
2024-09-24 01:35:27 +1000

    fix-231 Co-authored-by: butter1011 <luckybutter322982@gmail.com> Co-authored-by: topdeveloper1219 <kanjirohonda@gmail.com>

--- client/src/main.ts
+++ client/src/main.ts
@@ -31,7 +31,7 @@ if (platform !== 'unknown') {
       document.body.classList.add('is-material')
       break
     case 'ios':
-rsc
+rscdsf
     case 'macos':
 
       document.body.classList.add('is-apple')
